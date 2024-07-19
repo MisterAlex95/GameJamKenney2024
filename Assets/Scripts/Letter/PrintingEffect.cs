270c8997e3eeb24e877292c758bdbf4580264088
MisterAlex <alex.dana2013@gmail.com>
2024-07-19 21:39:00 +0200

    Add letter & printing effect

---
+++ Assets/Scripts/Letter/PrintingEffect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Letter
+{
+    public class PrintingEffect : MonoBehaviour
+    {
+        public float speed = 0.1f;
+        private string _text;
+        private TMPro.TextMeshProUGUI _textComponent;
+
+        private void Start()
+        {
+            _textComponent = GetComponent<TMPro.TextMeshProUGUI>();
+            _text = _textComponent.text;
+            _textComponent.text = "";
+            StartCoroutine(PrintText());
+        }
+
+        private System.Collections.IEnumerator PrintText()
+        {
+            foreach (var t in _text)
+            {
+                _textComponent.text += t;
+
+                yield return new WaitForSeconds(speed);
+            }
+        }
+    }
+}
