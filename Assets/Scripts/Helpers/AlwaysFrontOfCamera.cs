7d6befc2805497ce5636426be01813acf598386f
MisterAlex <alex.dana2013@gmail.com>
2024-07-19 21:06:19 +0200

    add full game

---
+++ Assets/Scripts/Helpers/AlwaysFrontOfCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Helpers
+{
+    public class AlwaysFrontOfCamera : MonoBehaviour
+    {
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            // Make the object always face the camera
+            transform.LookAt(_camera.transform);
+        }
+    }
+}
