01ebb1c7aa49078680e7b53cc575eef367035df2
MisterAlex <alex.dana2013@gmail.com>
2024-07-20 23:43:58 +0200

    Add introduction scene

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Journal;
+using Sound;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Core
 {
     public class GameManager : MonoBehaviour
     {
+        public GameObject blackScreen;
+        private Image _blackScreenImage;
+        private TMP_Text _blackScreenText;
+
         private readonly Dictionary<string, int> _characterState = new();
         public static GameManager Instance { get; private set; }
 
@@ -22,11 +31,82 @@ namespace Core
             }
         }
 
+        private void Start()
+        {
+            SetMoveDisabled(true);
+            _blackScreenImage = blackScreen.GetComponent<Image>();
+            _blackScreenText = blackScreen.GetComponentInChildren<TMP_Text>();
+            StartCoroutine(Introduction());
+        }
+
+        #region Transitions
+
+        private IEnumerator Introduction()
+        {
+            _blackScreenText.color = new Color(1, 1, 1, 0);
+            _blackScreenText.text = "Later the same day...";
+
+            blackScreen.SetActive(true);
+
+            // BlackScreen for 1sec
+            yield return new WaitForSeconds(1f);
+            float time = 0;
+
+            // Display Message
+            while (time < 1f)
+            {
+                time += Time.deltaTime;
+                _blackScreenText.color = new Color(1, 1, 1, 0 + time);
+                yield return null;
+            }
+
+            time = 0;
+            // Remove Message
+            while (time < 2f)
+            {
+                time += Time.deltaTime;
+                _blackScreenText.color = new Color(1, 1, 1, 1 - time / 2f);
+                yield return null;
+            }
+
+            // Bell Door
+            SoundManager.Instance.PlaySound("belldoor");
+            yield return new WaitWhile(SoundManager.Instance.IsPlaying);
+
+            // Walk to the door
+            SoundManager.Instance.PlaySound("walking");
+            yield return new WaitWhile(SoundManager.Instance.IsPlaying);
+
+            // Door
+            SoundManager.Instance.PlaySound("opendoor");
+            yield return new WaitWhile(SoundManager.Instance.IsPlaying);
+
+            time = 0;
+            while (time < 3f)
+            {
+                time += Time.deltaTime;
+                _blackScreenImage.color = new Color(0, 0, 0, 1 - time / 2f);
+                yield return null;
+            }
+
+            blackScreen.SetActive(false);
+        }
+
+        #endregion
+
         #region Global
 
         public bool CanInteract()
         {
             return !Dialog.DialogManager.Instance.IsDialogActive && !JournalManager.Instance.IsJournalActive;
+        }
+
+        public void SetMoveDisabled(bool isDisabled)
+        {
+            foreach (var arrowMove in FindObjectsOfType<ArrowMove>())
+            {
+                arrowMove.gameObject.SetActive(!isDisabled);
+            }
         }
 
         #endregion
---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sound
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public List<AudioClip> clips = new();
+        public static SoundManager Instance { get; private set; }
+
+        private bool _isPlaying;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void PlaySound(string clipName)
+        {
+            var clip = clips.Find(c => c.name == clipName);
+            if (UnityEngine.Camera.main == null) return;
+
+            if (_isPlaying) return;
+            AudioSource.PlayClipAtPoint(clip, UnityEngine.Camera.main.transform.position);
+            _isPlaying = true;
+
+            StartCoroutine(ResetIsPlaying(clip.length));
+        }
+
+        private IEnumerator ResetIsPlaying(float duration)
+        {
+            yield return new WaitForSeconds(duration);
+            _isPlaying = false;
+        }
+
+        public bool IsPlaying()
+        {
+            return _isPlaying;
+        }
+    }
+}
