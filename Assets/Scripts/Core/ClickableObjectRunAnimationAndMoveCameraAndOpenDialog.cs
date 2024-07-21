ef83dc1f4448ad96fe1dcdcc7c5959734062e733
MisterAlex <alex.dana2013@gmail.com>
2024-07-21 13:50:52 +0200

    Add fridge dialog

---
+++ Assets/Scripts/Core/ClickableObjectRunAnimationAndMoveCameraAndOpenDialog.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Camera;
+using Character;
+using Dialog;
+using UnityEngine;
+
+namespace Core
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class ClickableObjectRunAnimationAndMoveCameraAndOpenDialog : MonoBehaviour
+    {
+        public List<CameraPositionName> canBeTriggerFromCameraPositions;
+        public CameraPositionName cameraPositionName;
+        public DialogData dialogData;
+        private bool _alreadyTriggered = false;
+        public string triggerName;
+        public bool state = false;
+
+        private void Start()
+        {
+            GetComponent<BoxCollider>().isTrigger = true;
+        }
+
+        public void OnMouseDown()
+        {
+            if (!GameManager.Instance.CanInteract()) return;
+            if (!canBeTriggerFromCameraPositions.Contains(CameraManager.Instance().GetCameraPosition())) return;
+            CameraManager.Instance().SetCameraPosition(cameraPositionName);
+            if (triggerName == null) return;
+            state = !state;
+            GetComponent<Animator>().SetBool(triggerName, state);
+
+            if (!dialogData.looping && _alreadyTriggered) return;
+            if (dialogData.enableDialogAtStage > GameManager.Instance.GetCharacterState(CharacterName.Camden)) return;
+
+            DialogManager.Instance.StartDialog(new Dialog.Dialog(dialogData));
+            _alreadyTriggered = true;
+        }
+    }
+}
